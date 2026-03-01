714c5af81061291180684ec362ecab5d65d27614
kaito hino <n041969944@gmail.com>
2026-03-01 15:31:58 +0900

    feat: メニュー画面

---
+++ src/main/java/controller/manager/Menu.java
@@ -0,0 +1,22 @@
+package controller.manager;
+
+import java.io.IOException;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+// メニュークラス
+
+@WebServlet(urlPatterns={"/menu"})
+public class Menu extends HttpServlet {
+	public void doGet (
+		HttpServletRequest request, HttpServletResponse response
+	) throws ServletException, IOException {
+		
+		request.getRequestDispatcher("WEB-INF/jsp/manager/menu.jsp")
+			.forward(request, response);
+	}
+}
---
+++ src/main/webapp/WEB-INF/jsp/manager/menu.jsp
@@ -0,0 +1,12 @@
+<%@page contentType="text/html; charset=UTF-8" %>
+<%@include file="../common/header.html" %>
+
+<p>メニュー画面</p>
+
+<a href="list">学生一覧</a>
+<a href="search/input.jsp">学生検索</a>
+<a href="register/input.jsp">学生追加</a>
+<a href="delete/input.jsp">学生削除</a>
+<a href="update/input.jsp">学生更新</a>
+
+<%@include file="../common/footer.html" %>
